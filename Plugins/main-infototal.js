8a5001a9413bc212cf3a2a7020654ba103278baf
Kristylolu <lunitabot06@gmail.com>
2024-12-01 16:14:23 -0700

    Add files via upload

---
+++ Plugins/main-infototal.js
@@ -0,0 +1,11 @@
+let handler = async (m, { conn }) => {
+let totalf = Object.values(global.plugins).filter(
+    (v) => v.help && v.tags
+  ).length;
+conn.reply(m.chat, `*⭐ Total de Funciones* : ${totalf}`,m)
+}
+
+handler.help = ['totalfunciones']
+handler.tags = ['main']
+handler.command = ['totalfunciones', 'plugins']
+export default handler 
